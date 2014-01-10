b1eb84816c5162ca9888588bf69f8106ee02ed8a
Ben Howden <bhowden@fkasoft.net>
2014-01-10 12:53:52 -0600

    Update Paper.java
    set Paper props

--- src/main/java/org/thonk/entities/Paper.java
+++ src/main/java/org/thonk/entities/Paper.java
@@ -7,6 +7,7 @@
 package org.thonk.entities;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.persistence.Id;
 
 /**
  *
@@ -15,14 +16,24 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Paper {
     
-    private String dummyField;
+    @Id
+    private Long id;
+    private String url;
     
-    public void setDummyField(String dummyField) {
-        this.dummyField = dummyField;
+    public void setId (Long id) {
+        this.id = id;
     }
     
-    public String getDummyField() {
-        return dummyField;
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public String getUrl() {
+        return this.url;
     }
     
 }
