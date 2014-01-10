7f1a4e959b930a9fe904d8a798f0367aef0b66a0
Ben Howden <bhowden@fkasoft.net>
2014-01-10 12:32:38 -0600

    Update Category.java
    make category props

--- src/main/java/org/thonk/entities/Category.java
+++ src/main/java/org/thonk/entities/Category.java
@@ -7,6 +7,7 @@
 package org.thonk.entities;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.persistence.Id;
 
 /**
  *
@@ -15,14 +16,25 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Category {
     
+    @Id
     private Long id;
+    private Long parentCategory;
     
     public void setId(Long id) {
         this.id = id;
     }
     
     public Long getId() {
-        return id;
+        return this.id;
     }
+    
+    public void setParentCategory(Long parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+    
+    public Long getParentCategory() {
+        return this.parentCategory
+    }
+    
     
 }
