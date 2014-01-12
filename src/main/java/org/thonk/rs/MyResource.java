f03486f1b7fd59880c4bcaf360796501b0ee4da0
Ubuntu <ubuntu@ip-172-31-25-58.ec2.internal>
2014-01-12 06:31:09 -0600

    ejb inject rs resource

--- src/main/java/org/thonk/rs/MyResource.java
+++ src/main/java/org/thonk/rs/MyResource.java
@@ -1,22 +1,25 @@
 package org.thonk.rs;
 
+import javax.ejb.Stateless;
+import javax.ejb.EJB;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.thonk.ejb.MongoBean;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
+@Stateless
 @Path("/")
 public class MyResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent to
-     * the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
+    @EJB
+    MongoBean mongodb;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
