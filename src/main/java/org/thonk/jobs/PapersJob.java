386a111c5b4d49cdade8a455a030119f05b68925
Ubuntu <ubuntu@ip-172-31-25-58.ec2.internal>
2014-01-12 18:29:48 -0600

    papers job skeleton

---
+++ src/main/java/org/thonk/jobs/PapersJob.java
@@ -0,0 +1,13 @@
+package org.thonk.jobs;
+
+import 
+// this class pulls papers from various sources
+// and inserts themself in the DB
+public class PapersJob implements Runnable {
+
+    @Override
+    public void run() {
+    	System.out.println("TODO");
+    }
+
+}
