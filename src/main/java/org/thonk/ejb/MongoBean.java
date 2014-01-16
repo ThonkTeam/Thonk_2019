8197d076a4abe2f290a0e1a5ca8c4fb8e25d03d3
Ben Howden <bhowden@razemedia.com>
2014-01-16 13:34:33 -0600

    blah

--- src/main/java/org/thonk/ejb/MongoBean.java
+++ src/main/java/org/thonk/ejb/MongoBean.java
@@ -1,14 +1,29 @@
 package org.thonk.ejb;
 
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+
+import com.mongodb.MongoClient;
+import com.mongodb.DB;
+import com.mongodb.ServerAddress;
 
 import org.thonk.entities.*;
 
 @Stateless
 public class MongoBean {
 
-	public Category getCategoryById(Long id) {
-		return new Category();
-	}
+    private final static Logger LOGGER = Logger.getLogger(MyClass.class.getName());
+
+    MongoClient mongo;
+    DB db; 
+
+    public MongoBean() {
+        mongo = new MongoClient(new ServerAddress("localhost", 27017));
+        db = mongo.getDB("thonk");
+    }
+
+    public Category getCategoryById(Long id) {
+        return new Category();
+    }
 
 }
