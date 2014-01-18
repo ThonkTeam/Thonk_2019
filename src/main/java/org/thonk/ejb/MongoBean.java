4968988aa6f985e27a0c9df2e45d115406ea06f0
Ubuntu <ubuntu@ip-172-31-25-58.ec2.internal>
2014-01-18 00:13:57 -0600

    mongo connection error handling in ejb constructor

--- src/main/java/org/thonk/ejb/MongoBean.java
+++ src/main/java/org/thonk/ejb/MongoBean.java
@@ -2,6 +2,7 @@ package org.thonk.ejb;
 
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import java.net.UnknownHostException;
 
 import com.mongodb.MongoClient;
 import com.mongodb.DB;
@@ -12,14 +13,18 @@ import org.thonk.entities.*;
 @Stateless
 public class MongoBean {
 
-    private final static Logger LOGGER = Logger.getLogger(MyClass.class.getName());
+    private final static Logger log = Logger.getLogger(MongoBean.class.getName());
 
     MongoClient mongo;
     DB db; 
 
     public MongoBean() {
-        mongo = new MongoClient(new ServerAddress("localhost", 27017));
-        db = mongo.getDB("thonk");
+        try {
+            mongo = new MongoClient(new ServerAddress("localhost", 27017));
+            db = mongo.getDB("thonk");
+        } catch(UnknownHostException ex)  {
+            log.severe(ex.getMessage());
+        }
     }
 
     public Category getCategoryById(Long id) {
