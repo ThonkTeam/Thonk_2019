f3d752283087959e8c0ae3b5216317e28e607989
Ubuntu <ubuntu@ip-172-31-25-58.ec2.internal>
2014-01-25 10:48:41 -0600

    rest create/read/delete categories

--- src/main/java/org/thonk/rs/ThonkAPI.java
+++ src/main/java/org/thonk/rs/ThonkAPI.java
@@ -4,11 +4,17 @@ import javax.ejb.Stateless;
 import javax.ejb.EJB;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.thonk.ejb.MongoBean;
+import org.thonk.entities.*;
 
 @Stateless
 @Path("/")
@@ -17,13 +23,27 @@ public class ThonkAPI {
     @EJB
     MongoBean mongodb;
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        String sent = "Any sensual bliss in the world,"
-	              +" any heavenly bliss,"
-                      +" isn't worth one sixteenth-sixteenth"
-                      +" of the bliss of the ending of craving.";
-	return sent; 
+    @POST
+    @Path("/category")
+    public Response createCategory(Category cat){
+        String id = mongodb.createCategory(cat);
+        return Response.status(200).entity(id).build();
     }
+
+    @GET
+    @Path("/category/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Category readCategory(@PathParam("id") String id){
+        return mongodb.readCategory(id);
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteCategory(@PathParam("id") String id){
+        mongodb.deleteCategory(id);
+        return Response.status(200).build();
+    }
+
+
+
 }
